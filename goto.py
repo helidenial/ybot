ccd3b7be6d651ec6b886926005511b2cee7d5acd
aman-1004 <2020ceb1004@iitrpr.ac.in>
2021-06-06 02:33:54 +0530

    First commit

---
+++ goto.py
@@ -0,0 +1,33 @@
+from time import sleep
+from selenium import webdriver
+from random import randrange as rand
+from selenium.webdriver.chrome.webdriver import WebDriver
+import os
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+
+def visit(url, loop, duration=10):
+    driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
+
+    try:
+        for i in range(loop):
+            try:
+                driver.get(url)
+                print(url, end=" ")
+                sleep(rand(duration, duration+10))
+                print(driver.page_source)
+                print('########################################')
+                print("visited")
+            except:
+                driver = webdriver.Chrome()
+    except:
+        pass
+    
+    finally:
+        if isinstance(driver, WebDriver) != True:
+            driver.quit()
+            
---
+++ ybot.py
@@ -0,0 +1,29 @@
+from weakref import WeakKeyDictionary
+from flask import Flask, render_template, request
+import threading
+from goto import visit
+
+app = Flask(__name__)
+
+
+@app.route('/',methods =["GET"])
+def home():
+    print(request.args)
+    if len(request.args) == 0:
+        pass
+    else:
+        url = request.args.get('url')
+        views = int(request.args.get('noOfViews'))
+        threads = int(request.args.get('threads'))
+        duration = int(request.args.get('duration'))
+        for i in range(0,threads):    
+            threading.Thread(target=visit, args=(url, views, duration, )).start()
+
+    return render_template('index.html')
+
+app.run(debug=True)
+
+
+
+
+
